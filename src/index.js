84e51943f015ef1137b8b79f7ceed014a326e272
gin <73906888+gingkapls@users.noreply.github.com>
2024-04-11 18:16:21 +0530

    Add html skeleton and basic styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import data from "./data.json";
 import Task from "./components/Task";
 import Project from "./components/Project";
+import "./style.css";
 
 const projectList = data.map((project) => new Project(project));
 
